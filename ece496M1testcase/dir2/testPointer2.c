d2c16a0eadecd18468a12c912a9f71e3b692281f
yilinchenn <35321176+yilinchenn@users.noreply.github.com>
2018-01-11 22:46:03 -0500

    Enhance M1 test cases

---
+++ ece496M1testcase/dir2/testPointer2.c
@@ -0,0 +1,41 @@
+//
+//  testPointer2.c
+//  testSourceCode
+//
+//  Created by Yilin Chen on 2018-01-11.
+//  Copyright © 2018 Yilin Chen. All rights reserved.
+//
+
+#include "testPointer2.h"
+#include "testPointer.h"
+
+float testVoidPtr(void *ptr)
+{
+    struct foo* fooPtr = (struct foo*)ptr;
+    float ret = -1;
+    if (fooPtr->foo1 * fooPtr->foo2 > 100)
+        ret = (fooPtr->foo1 + fooPtr->foo2)/10;
+    else
+        ret = fooPtr->foo1 + fooPtr->foo2;
+    return ret;
+}
+
+int testPtrs(struct foo *fooPtr, char* arr)
+{
+    if (*(arr + fooPtr->foo1) == *(arr + fooPtr->foo2))
+        return 1;
+    else
+        return -1;
+}
+
+char testArray(char arr[])
+{
+    int i = 0;
+    char ret = arr[0];
+    while (arr[i] != 'T')
+    {
+        ret = arr[i-1];
+        i++;
+    }
+    return ret;
+}
--- ece496M1testcase/dir2/testPointer2.h
+++ ece496M1testcase/dir2/testPointer2.h
@@ -1,9 +1,21 @@
-#include "../dir1/testPointer.h"
+//
+//  testPointer2.h
+//  testSourceCode
+//
+//  Created by Yilin Chen on 2018-01-11.
+//  Copyright © 2018 Yilin Chen. All rights reserved.
+//
 
-int testFuncPtr(int (*funcp) (int, int), int a);
+#ifndef testPointer2_h
+#define testPointer2_h
+
+#include <stdio.h>
+#include "../dir1/testPointer.h"
 
 float testVoidPtr(void *ptr);
 
 int testPtrs(struct foo *fooPtr, char* arr);
 
 char testArray(char arr[]);
+
+#endif /* testPointer2_h */
